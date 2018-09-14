c0640c5b2e91570c98584b5899aa91dfa51cfdfb
billowbashir <billowbashir@gmail.com>
2018-09-14 17:56:57 +0300

    change files

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import db
-# from . import login_manager
+from . import login_manager
 
-# 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -27,3 +27,8 @@ class User(UserMixin,db.Model):
         return check_password_hash(self.pass_secure,password)
     def __repr__(self):
         return f'User {self.username}'
+class Blog(db.Model):
+    __tablename__='blogs'
+    id=db.Column(db.Integer,primary_key=True)
+    topic=db.Column(db.String(255))
+    blog=db.Column(db.String(255))
